4839577f3973ece3711df97b64da931b1b5440ef
dracero <diego.racero@gmail.com>
2022-10-07 12:00:41 -0300

    normalbien

--- src/Move.jsx
+++ src/Move.jsx
@@ -18,7 +18,7 @@ const momento = () =>{
 }
   
  return (
-    <group ref={group} {...props} dispose={null}  position = {[2, -5, -1]}>
+    <group ref={group} {...props} dispose={null}  position = {[2, 0, -1]}>
     <group name="Scene">
         <group
           name="Plane"
@@ -26,97 +26,97 @@ const momento = () =>{
           rotation={[1.58, 0.01, -1.56]}
         />
         <mesh
-          name="Cylinder001" //eje con las dos ruedas
+          name="Cylinder001" //eje con ruedas
           castShadow
           receiveShadow
           geometry={nodes.Cylinder001.geometry}
           material={materials["Material.004"]}
-          position={[-1.91, 5.38, -0.29]}
+          position={[-1.91, 0.72, -0.29]}
           rotation={[1.58, 0.01, -1.56]}
           scale={[1, 0.48, 1]}
         />
-        {torque ?<mesh 
-                      name="Cylinder" //torque
-                      castShadow
-                      receiveShadow
-                      geometry={nodes.Cylinder.geometry}
-                      material={materials["Material.007"]}
-                      position={[1.64, 5.39, -0.3]}
-                      rotation={[0.01, -0.02, -1.57]}
-                      scale={[1.04, 1.16, 1]}
-                /> : null
+        {torque ?<mesh //torque
+          name="Cylinder"
+          castShadow
+          receiveShadow
+          geometry={nodes.Cylinder.geometry}
+          material={materials["Material.007"]}
+          position={[1.64, 0.73, -0.3]}
+          rotation={[0.01, -0.02, -1.57]}
+          scale={[1.04, 1.16, 1]}
+        /> : null
         }
         {peso ?
          <mesh
-          name="Cylinder002" //vector que representa el peso
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder002.geometry}
-          material={materials["Material.006"]}
-          position={[-1.81, 3.88, -0.33]}
-          rotation={[-0.08, 1.15, -3.06]}
-          scale={[1.04, 1.16, 1]}
-        />: 
+         name="Cylinder002" //peso
+         castShadow
+         receiveShadow
+         geometry={nodes.Cylinder002.geometry}
+         material={materials["Material.006"]}
+         position={[-1.81, -0.77, -0.33]}
+         rotation={[-0.08, 1.15, -3.06]}
+         scale={[1.04, 1.16, 1]}
+       />: 
         null}
         {normal ? 
         <group>
         <mesh
-          name="Cylinder004"// una normal
+          name="Cylinder004" //normal
           castShadow
           receiveShadow
           geometry={nodes.Cylinder004.geometry}
           material={materials["Material.005"]}
-          position={[-4.75, 6.76, 0.24]}
+          position={[-4.75, -0.04, -0.49]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />,
         <mesh
-          name="Cylinder011"//una normal
+          name="Cylinder011" //normal
           castShadow
           receiveShadow
           geometry={nodes.Cylinder011.geometry}
           material={materials["Material.005"]}
-          position={[0.67, 6.69, 0.23]}
+          position={[0.63, -0.01, -0.25]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />,
         <mesh
-          name="Cylinder006"//una normal
+          name="Cylinder006" //normal
           castShadow
           receiveShadow
           geometry={nodes.Cylinder006.geometry}
           material={materials["Material.001"]}
-          position={[-4.75, 6.76, 0.24]}
+          position={[-4.75, -0.04, -0.49]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />,
         <mesh
-          name="Cylinder008" //normal
+          name="Cylinder008" //nomal
           castShadow
           receiveShadow
           geometry={nodes.Cylinder008.geometry}
           material={materials["Material.008"]}
-          position={[-4.75, 6.76, 0.24]}
+          position={[-4.75, -0.04, -0.49]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />,
-         <mesh
-          name="Cylinder009" // normal
+                 <mesh
+          name="Cylinder009" //normal
           castShadow
           receiveShadow
           geometry={nodes.Cylinder009.geometry}
           material={materials["Material.009"]}
-          position={[-4.75, 6.76, 0.24]}
+          position={[-4.75, -0.04, -0.49]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />,
         <mesh
-          name="Cylinder010" //normal
+          name="Cylinder010" //norma
           castShadow
           receiveShadow
           geometry={nodes.Cylinder010.geometry}
           material={materials["Material.010"]}
-          position={[-4.75, 6.76, 0.24]}
+          position={[-4.75, -0.04, -0.49]}
           rotation={[0.41, 0.01, 0.02]}
           scale={[1.04, 1, 0.97]}
         />
@@ -124,32 +124,32 @@ const momento = () =>{
         {friccion ?
         <group>
         <mesh
-          name="Cylinder007"//rosamiento
+          name="Cylinder007" //rozamiento
           castShadow
           receiveShadow
           geometry={nodes.Cylinder007.geometry}
           material={materials["Material.002"]}
-          position={[-4.48, 3.93, -2.12]}
+          position={[-4.48, -0.73, -2.12]}
           rotation={[-1.12, 0.01, -0.04]}
           scale={[1.04, 1.16, 1]}
         />
         <mesh
-          name="Cylinder005"//rosamiento
+          name="Cylinder005" //rosamiento
           castShadow
           receiveShadow
           geometry={nodes.Cylinder005.geometry}
           material={materials["Material.003"]}
-          position={[-4.48, 3.93, -2.12]}
+          position={[-4.48, -0.73, -2.12]}
           rotation={[-1.12, 0.01, -0.04]}
           scale={[1.04, 1.16, 1]}
         />
-        <mesh
-          name="Cylinder003" //un rosamiento
+         <mesh
+          name="Cylinder003" //rosamiento
           castShadow
           receiveShadow
           geometry={nodes.Cylinder003.geometry}
           material={materials["Material.003"]}
-          position={[0.52, 3.9, -1.86]}
+          position={[0.52, -0.76, -1.86]}
           rotation={[-1.12, 0.01, -0.04]}
           scale={[1.04, 1.16, 1]}
         />
