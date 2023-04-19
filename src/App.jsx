9627fddb2d04049931918eb13ad18f10fc804dfd
dracero <diego.racero@gmail.com>
2023-04-19 17:13:53 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,18 @@
-import React, { useEffect, useState } from "react";
 import Move from "./Move";
 import { OrbitControls} from "@react-three/drei";
 import { VRCanvas } from "@react-three/xr";
-import { Button } from "@mui/material";
-import {useDispatch} from 'react-redux'
-import allActions from './actions/allActions'
 import {Provider} from 'react-redux'
 import store from "./store/store"
 import { Physics} from "@react-three/cannon"
 
 
 const App  = () => {
-  const dispatch = useDispatch()
-  const [nvalue,setNValue]=useState(false)
-  const [fvalue,setFValue]=useState(false)
-  const [wvalue,setWValue]=useState(false)
-  const [tvalue,setTValue]=useState(false)
-  
-  useEffect(() => {
-     dispatch(allActions.vectorActions.friction(fvalue))
-     dispatch(allActions.vectorActions.normal(nvalue))
-     dispatch(allActions.vectorActions.weight(wvalue))
-     dispatch(allActions.vectorActions.torque(tvalue))
-  })
-  const setfriction = () =>{
-    setFValue(!fvalue)
-    dispatch(allActions.vectorActions.friction(fvalue))
-  }
-  const setnormal = () =>{
-    setNValue(!nvalue)
-    dispatch(allActions.vectorActions.normal(nvalue))
-  }
-  const setweight = () =>{
-    setWValue(!wvalue)
-    dispatch(allActions.vectorActions.weight(wvalue))
-  }
-  const settorque = () =>{
-    setTValue(!tvalue)
-    dispatch(allActions.vectorActions.torque(tvalue))
-  }
- 
+   
   
   return (  
   <div className="boardCanvas"
   style={{ width: "80vw", height: "40vw" }} > 
-  <Button variant="outlined" onClick={setfriction}>Rozamiento</Button>
-  <p></p>
-  <Button variant="outlined" onClick={setnormal}>Normal</Button>
-  <p></p>
-  <Button variant="outlined" onClick={setweight}>Peso</Button>
-  <p></p>
-  <Button variant="outlined" onClick={settorque}>Torque</Button>
-  <VRCanvas>
+  <VRCanvas referenceSpace="local">
     <ambientLight />
       <spotLight />
       <OrbitControls />
--- src/Move.jsx
+++ src/Move.jsx
@@ -1,181 +1,97 @@
-import React, { useRef } from "react";
-import { useLoader, useFrame } from "@react-three/fiber";
-import {useGLTF } from "@react-three/drei";
-import {useSelector} from 'react-redux'
-import { useController } from "@react-three/xr"
-import { useBox } from "@react-three/cannon"
-import * as THREE from "three"
+import React, { useRef, useState, useEffect } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
-
-const Move  = () => {
-  const model = useRef();
-  const rightController = useController("right")
-  const [ref, api] = useBox(() => ({ type: "Kinematic" }))
-  //Acá importa el color y las geometrías, para cambiar el color hay que cambiar el tipo de material
-  const { nodes, materials} = useGLTF("/ejeConVector.glb");
-  //Acá estoy usando Hooks de Redux
-  const normal = useSelector(state => state.vector.disnor)
-  const  torque = useSelector(state => state.vector.distor)
-  const  peso = useSelector(state => state.vector.diswei)
-  const  friccion = useSelector(state => state.vector.disfric)
-  //const { actions } = useAnimations(animations, group);
-  useFrame((state) => {
-    if (!rightController) {
-      return
+const Move = (props) => {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/cupula.glb");
+  const { actions } = useAnimations(animations, group);
+  let [removeMesh, setRemoveMesh] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
+  
+  const removeMeshAfterFrames = () => {
+    if (isPlaying) {
+      console.log("llega")
+      setIsPlaying(false)
+      actions["movimiento"].paused = true;
+    } else {
+      setIsPlaying(true)
+      actions["movimiento"].paused = false;
     }
-    const { grip: controller } = rightController
-    const forward = new THREE.Vector3(1, 1, 1)
-    forward.applyQuaternion(controller.quaternion)
-    const position = new THREE.Vector3().copy(controller.position).add(forward)
-    api.position.set(position.x, position.y, position.z)
-    api.rotation.set(controller.rotation.x, controller.rotation.y, controller.rotation.z)
-  })
+  }
+
+  const reinicioMove = () => {
+     actions["movimiento"].reset();
+     setRemoveMesh(false)
+  }
+
+  useEffect(() => {
+    actions["movimiento"].play();
+  },[]);
 
  return (
- <mesh ref={ref} dispose={null} scale={[1, 1, 1]}> 
-  <group >
-    <group name="Scene" dispose={null} position = {[2, 0, -5]} ref={model}>
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
         <group
-          name="Plane"
-          position={[-5.29, 10.59, -7.28]}
-          rotation={[1.58, 0.01, -1.56]}
-        />
+          name="NurbsPath001"
+          position={[0, 1.8, -3.27]}
+          rotation={[0, 1.33, 0]}
+          scale={[1.44, 1, 1.03]}
+        >
+          <group
+            name="Sphere003"
+            position={[-2.61, -0.35, 0.76]}
+            rotation={[-2.39, 0.03, -1.49]}
+            scale={[0.98, 0.7, 0.98]}
+          >
+            {!removeMesh && (
+              <mesh
+                name="Sphere006"
+                castShadow
+                receiveShadow
+                geometry={nodes.Sphere006.geometry}
+                material={materials["Material.004"]}
+              />
+            )}
+            <mesh
+              name="Sphere006_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.Sphere006_1.geometry}
+              material={materials.bola}
+            />
+            {!removeMesh && (
+              <mesh
+                name="Sphere006_2"
+                castShadow
+                receiveShadow
+                geometry={nodes.Sphere006_2.geometry}
+                material={materials["Material.003"]}
+              />
+            )}
+          </group>
+        </group>
         <mesh
-          name="Cylinder001" //eje con ruedas
+          name="Sphere"
           castShadow
           receiveShadow
-          geometry={nodes.Cylinder001.geometry}
-          material={materials["Material.004"]}
-          position={[-1.91, 0.72, -0.29]}
-          rotation={[1.58, 0.01, -1.56]}
-          scale={[1, 0.48, 1]}
-        />
-        {torque ?<mesh //torque
-          name="Cylinder"
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder.geometry}
-          material={materials["Material.007"]}
-          position={[1.64, 0.73, -0.3]}
-          rotation={[0.01, -0.02, -1.57]}
-          scale={[1.04, 1.16, 1]}
-        /> : null
-        }
-        {peso ?
-         <mesh
-         name="Cylinder002" //peso
-         castShadow
-         receiveShadow
-         geometry={nodes.Cylinder002.geometry}
-         material={materials["Material.006"]}
-         position={[-1.81, -0.77, -0.33]}
-         rotation={[-0.08, 1.15, -3.06]}
-         scale={[1.04, 1.16, 1]}
-       />: 
-        null}
-        {normal ? 
-        <group>
-        <mesh
-          name="Cylinder004" //normal
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder004.geometry}
-          material={materials["Material.005"]}
-          position={[-4.75, -0.04, -0.49]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
-        />,
-        <mesh
-          name="Cylinder011" //normal
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder011.geometry}
-          material={materials["Material.005"]}
-          position={[0.63, -0.01, -0.25]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
-        />,
-        <mesh
-          name="Cylinder006" //normal
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder006.geometry}
+          geometry={nodes.Sphere.geometry}
           material={materials["Material.001"]}
-          position={[-4.75, -0.04, -0.49]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
-        />,
-        <mesh
-          name="Cylinder008" //nomal
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder008.geometry}
-          material={materials["Material.008"]}
-          position={[-4.75, -0.04, -0.49]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
-        />,
-                 <mesh
-          name="Cylinder009" //normal
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder009.geometry}
-          material={materials["Material.009"]}
-          position={[-4.75, -0.04, -0.49]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
-        />,
-        <mesh
-          name="Cylinder010" //norma
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder010.geometry}
-          material={materials["Material.010"]}
-          position={[-4.75, -0.04, -0.49]}
-          rotation={[0.41, 0.01, 0.02]}
-          scale={[1.04, 1, 0.97]}
+          position={[0, -4.7, 0.26]}
+          scale={2}
         />
-        </group>: null}
-        {friccion ?
-        <group>
-        <mesh
-          name="Cylinder007" //rozamiento
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder007.geometry}
-          material={materials["Material.002"]}
-          position={[-4.48, -0.73, -2.12]}
-          rotation={[-1.12, 0.01, -0.04]}
-          scale={[1.04, 1.16, 1]}
-        />
-        <mesh
-          name="Cylinder005" //rosamiento
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder005.geometry}
-          material={materials["Material.003"]}
-          position={[-4.48, -0.73, -2.12]}
-          rotation={[-1.12, 0.01, -0.04]}
-          scale={[1.04, 1.16, 1]}
-        />
-         <mesh
-          name="Cylinder003" //rosamiento
-          castShadow
-          receiveShadow
-          geometry={nodes.Cylinder003.geometry}
-          material={materials["Material.003"]}
-          position={[0.52, -0.76, -1.86]}
-          rotation={[-1.12, 0.01, -0.04]}
-          scale={[1.04, 1.16, 1]}
-        />
-       </group>:
-       null} 
-      </group> 
-  </group> 
-  </mesh>
-  );}
+      </group>
+      <Html position={[0, 0, 0]}>
+        <div style={{ position: "absolute", top: 10, left: 10 }}>
+          <button onClick={removeMeshAfterFrames}>Movement</button>
+          <button onClick={() => setRemoveMesh(true)}>Salida</button>
+          <button onClick={reinicioMove}>Reinicio</button>
+        </div>
+      </Html>
+    </group>
+  );
+};
 
-useGLTF.preload("/ejeConVector.glb");
+useGLTF.preload("/cupula.glb");
 
-
-export default Move
+export default Move;
